6f847b1cf92acf907bcc47955aec7b39081c2f32
Harald Niemeczek <harald@niemeczek.at>
2017-02-22 00:41:13 +0100

    transplanted content (index.php)

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<?php
+include("assets/template/header.php");
+?>
+
+						<!--Begin Content -->  <br /><br />
+							<div>
+								<br /><br />
+					Was haben ein Sportlehrer, ein Student f&uuml;r Bauingenieurwesen, ein Pilot, ein Gartenbausch&uuml;ler, ein Mechatroniker und ein Pharmaziestudent gemeinsam?<br />
+                    <br />
+                    <br /><br />Sie haben es sich zum Ziel gesetzt die Stadt Wien im Sandbox Open-World-Spiel Minecraft als virtuelle Welt nachzubauen. 
+                    <br /><br />
+                    <img src="/assets/img/chunkyrender/Vienna-Donaukanal1.png" width="90%" alt="Wien Donaukanal" />
+                    <br /><br />Bei Minecraft, dessen Namen sich aus den englischen Begriffen f&uuml;r Abbauen und Gestalten bzw. Anfertigen zusammensetzt, handelt es sich um ein Computerspiel bei dem Spieler, allein oder auch gemeinsam online allerhand kreative Welten aus verschiedensten Bausteinen entstehen lassen k&ouml;nnen. 
+                    <br /><br />Ausgehend vom 1. Bezirk, der Inneren Stadt, werden nach und nach Wahrzeichen, Stra&szlig;en und Geb&auml;ude in penibler Detailarbeit dem Original nachempfunden. Hierzu werden sowohl Satellitenaufnahmen, Pl&auml;ne und Kartenmaterial als auch vor Ort aufgenomme Fotos zur Hilfe gezogen. Im Zuge der n&auml;chsten Monate und Jahre werden immer mehr Gr&auml;tzl der Hauptstadt in dieser Art und Weise gebaut werden.
+                    <br /><br />
+                    <img src="/assets/img/chunkyrender/Vienna-Stephansdom1.png" width="90%" alt="Wien Stephansdom" />
+                    <br /><br />Um sich besser koordinieren zu k&ouml;nnen haben sich die kreativen K&ouml;pfe um Gr&uuml;nder Stefan Riedmann und Serveradminstrator Harald Niemeczek zum Verein Minecraft Vienna zusammengeschlossen.
+                    <br /><br />Seit Anfang 2015 unterst&uuml;tzt auch Wien-Kanal dieses innovative Projekt. Insbesondere jene Teile des Wiener Kanalnetzes, welche auch im Zuge der 3. Mann Tour besichtigt werden k&ouml;nnen, sowie Abschnitte des Wienflusses werden in die dreidimensionale virtuelle Welt aus unz&auml;hligen Bl&ouml;cken eingebunden.
+                    </p>      
+
+					<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+									
+								</p>
+
+
+                    <br /><br />
+								</p> 
+							</div>
+							<!--Hier CONTENT Ende-->
+	<?php
+	include("assets/template/footer.php");
+	?>	
+	
