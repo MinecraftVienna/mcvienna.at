3103770c6c44a274c1ad02b63ae400ac19699a08
Harald Niemeczek <postmaster@niemeczek.at>
2017-08-09 14:27:58 +0200

    Korrekturen aus issue #2

--- en/index.php
+++ en/index.php
@@ -5,17 +5,17 @@ include("assets/template/header.php");
 
 						<!--Begin Content -->  
 								<br /><br />
-					What do a PE teacher, a student of civil engineering, a Pilot, a student of computer science, a mechatronic technician and a student of pharmacy in common?<br />
+					What do a PE teacher, a student of civil engineering, a pilot, a student of computer science, a mechatronics engineer and a student of pharmacy have in common?<br />
                     <br /> 
-                    <br /><br />They are dedicated to build a full-scale replica of the city Vienna in the sandbox open-world-game Minecraft. 
+                    <br /><br />They are determined to build a full-scale replica of the city of Vienna in the sandbox open-world-game Minecraft. 
                     <br /><br />
                     <img src="/assets/img/chunkyrender/Vienna-Donaukanal1.png" width="90%" alt="Wien Donaukanal" />
-                    <br /><br />Minecraft is a computergame where players can - alone or together - construct online lots of creative worlds out of several different building blocks.
-                    <br /><br />Starting from the first district, also called "Innere Stadt", there are built landmarks, streets and buildings bit by bit in minute detail like the original. For that purpose sattelite images, maps as well as photos made at that places are used. In this manner there will be built more and more quarters of the capital city during the next months and years.
+                    <br /><br />Minecraft is a computer game where players can - alone or together - construct lots of creative worlds out of several different building blocks online.
+                    <br /><br />Starting from the first district called "Innere Stadt", landmarks, streets and buildings are getting built bit by bit in minute detail like the original. For that purpose sattelite images, maps as well as pictures took on site, are used. In this manner more and more quarters of the capital city will be built during the next months and years.
                     <br /><br />
                     <img src="/assets/img/chunkyrender/Vienna-Stephansdom1.png" width="90%" alt="Wien Stephansdom" />
-                    <br /><br />In order to be able to coordinate better, the creative minds around Founder Stefan Riedmann and Serveradministrator Harald Niemeczek teamed up to the registred association Minecraft Vienna.
-                    <br /><br />Since the beginning of 2015 Wien-Kanal is supporting this innovative Project. Especially those parts of the Viennese Canal Network that can be viewed at the 3rd Man Tour, as well as parts of the Wien River are included in the three-dimensional world made out of uncountable numbers of Blocks.
+                    <br /><br />In order to be able to coordinate better, the creative minds around founder Stefan Riedmann and Server administrator Harald Niemeczek teamed up to the registered association Minecraft Vienna.
+                    <br /><br />Since the beginning of 2015 Wien-Kanal is supporting this innovative project. Especially those parts of the Viennese Canal Network that can be viewed at the 3rd Man Tour, as well as parts of the Wien River are included in the three-dimensional world made from countless blocks.
                     <br /><br /><br /><br /><img src="/assets/img/chunkyrender/Vienna-MAK.png" width="90%" alt="Wien MAK" />
                     </p>      
 
